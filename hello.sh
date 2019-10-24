9d3dea2f987fd6f0fdb78cf0e133446828ffc660
ecat <qule520@126.com>
2019-10-24 23:02:03 +0800

    解决cowthink可能不存在的bug

--- hello.sh
+++ hello.sh
@@ -2,19 +2,19 @@
 # 你用cowsay打印Hello  $USER
 # 必须在新进程中调用，引入方式可能会造成shell整体推出
 
-if [ -z "$(which cowsay)" ]; then
+cowdo="cowsay"
+[ $((RANDOM % 2)) -eq 0 ] && cowdo="cowthink"
+
+if [ -z "$(which $cowdo)" ]; then
     echo Hello $USER
     exit
 fi
-cow_file=$(cowsay -l | grep -oE '/.*?:')
+cow_file=$($cowdo -l | grep -oE '/.*?:')
 cow_file=${cow_file%:}
-cnt=`ls ${cow_file}/*.cow | wc -l`
+cnt=$(ls ${cow_file}/*.cow | wc -l)
 declare -a cows
-for i in ${cow_file}/*.cow
-do
+for i in ${cow_file}/*.cow; do
     cows[${#cows[@]}]=$i
 done
-i=$[RANDOM % $cnt]
-cowdo="cowsay"
-[ $[RANDOM % 2] -eq 0 ] && cowdo="cowthink"
+i=$((RANDOM % $cnt))
 $cowdo -f "${cows[$i]}" "Hello $USER"
